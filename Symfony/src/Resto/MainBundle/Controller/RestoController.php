9dbb76c47d284d38fa695748598ae6c50c769847
thepape <romain.papelier@gmail.com>
2014-01-23 21:57:35 +0100

    panier compte ok

--- Symfony/src/Resto/MainBundle/Controller/RestoController.php
+++ Symfony/src/Resto/MainBundle/Controller/RestoController.php
@@ -11,18 +11,40 @@ use Resto\MainBundle\Entity\Plat;
 class RestoController extends Controller {
 
     public function indexAction() {
-        return $this->render('RestoMainBundle:Resto:index.html.twig');
+        if(isset($panier))
+        {
+             $panier = $this->getPanier();
+
+        $total = $this->totalPanier();
+        }
+        else
+        {
+            $total = 0;
+        }
+       
+        return $this->render('RestoMainBundle:Resto:index.html.twig', array('total' => $total));
     }
 
 
     public function themeAction($themeid)
     {
+        if(isset($panier))
+        {
+             $panier = $this->getPanier();
+
+        $total = $this->totalPanier();
+        }
+        else
+        {
+            $total = 0;
+        }
+
         if(($themeid == -1) || ($themeid == ''))
         {
             $repository = $this->getDoctrine()->getManager()->getRepository('RestoMainBundle:Theme');
             $themes = $repository->findAll();
 
-            return $this->render('RestoMainBundle:Resto:themes.html.twig', array('themes' => $themes));
+            return $this->render('RestoMainBundle:Resto:themes.html.twig', array('themes' => $themes, 'total' => $total));
         }
         else
         {
@@ -32,22 +54,34 @@ class RestoController extends Controller {
 
              $restos = $theme->getRestaurants();
 
-             return $this->render('RestoMainBundle:Resto:theme.html.twig', array('restos' => $restos));
+             return $this->render('RestoMainBundle:Resto:theme.html.twig', array('restos' => $restos, 'total' => $total));
 
         }
 
     }
 
     public function restoAction($restoid) {
+        if(isset($panier))
+        {
+             $panier = $this->getPanier();
+
+        $total = $this->totalPanier();
+        }
+        else
+        {
+            $total = 0;
+        }
 
         $repository = $this->getDoctrine()->getManager()->getRepository('RestoMainBundle:Restaurant');
         $resto = $repository->find($restoid);
         $plats = $resto->getPlats();
 
-    	return $this->render('RestoMainBundle:Resto:resto.html.twig', array('resto' => $resto, 'plats' => $plats));
+    	return $this->render('RestoMainBundle:Resto:resto.html.twig', array('resto' => $resto, 'plats' => $plats, 'total' => $total));
     }
 
     public function panierAction() {
+
+
 
         $panier = $this->getPanier();
 
@@ -70,12 +104,23 @@ class RestoController extends Controller {
 
     public function platAction($id)
     {
+        if(isset($panier))
+        {
+             $panier = $this->getPanier();
+
+        $total = $this->totalPanier();
+        }
+        else
+        {
+            $total = 0;
+        }
+
         if($id == -1)
         {
             $repository = $this->getDoctrine()->getManager()->getRepository('RestoMainBundle:Restaurant');
             $plats = $repository->getPlats();
 
-            return $this->render('RestoMainBundle:Resto:plats.html.twig', array('plats' => $plats));
+            return $this->render('RestoMainBundle:Resto:plats.html.twig', array('plats' => $plats, 'total' => $total));
         }
 
     }
@@ -113,10 +158,10 @@ class RestoController extends Controller {
 
         $session = $this->getRequest()->getSession();
         $session->set('panier', $panier);
-
+        $total = $this->totalPanier();
         
 
-        return $this->render('RestoMainBundle:Resto:resto.html.twig', array('resto' => $resto, 'plats' => $plats));
+        return $this->render('RestoMainBundle:Resto:resto.html.twig', array('resto' => $resto, 'plats' => $plats, 'total' => $total));
     }
 
     public function supprimeArticleAction($id)  
